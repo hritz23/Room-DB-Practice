d2f89426e5dddc11e7ea50043025bb731e28864d
HritikaNaik <hritikan232@gmail.com>
2024-06-26 14:21:39 +0530

    removed warnings

--- app/src/main/java/com/example/roompractice/MainActivity.kt
+++ app/src/main/java/com/example/roompractice/MainActivity.kt
@@ -5,11 +5,9 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.example.roompractice.databinding.ActivityMainBinding
